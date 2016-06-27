24305e6b0a7b7768e18bba7f394ac46c8d987269
brantje <brantje@gmail.com>
2016-06-27 22:11:05 +0200

    Add no-cache header

--- scripts/app.js
+++ scripts/app.js
@@ -91,7 +91,19 @@ angular
       return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
     }];
 
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
 
+    // Answer edited to include suggestions from comments
+    // because previous version of code introduced browser-related errors
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
   });
 
 String.prototype.capitalizeFirstLetter = function() {
