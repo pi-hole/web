8a8adaeb1dfb693e728c09946fafa3366f1c17de
brantje <brantje@gmail.com>
2016-06-27 20:01:29 +0200

    New files got spit out by a blackhole

---
+++ scripts/filters/bytefilter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name piholeAdminApp.directive:glow
+ * @description
+ * # glow
+ */
+angular.module('piholeAdminApp').filter('bytes', function() {
+  return function(bytes, precision) {
+    if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+    if (typeof precision === 'undefined') precision = 1;
+    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+      number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+  }
+});
