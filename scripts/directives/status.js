28f75b918b9f269a2d3d58f8bc84afc244835443
brantje <brantje@gmail.com>
2016-07-02 16:48:48 +0200

    Add translation support

--- scripts/directives/status.js
+++ scripts/directives/status.js
@@ -9,7 +9,7 @@
 angular.module('piholeAdminApp')
   .directive('status',[function () {
     return {
-      template: '<div></div>',
+      template: '<i ng-if="!ssstatus" class="fa fa-circle" style="color:#FF0000"></i><i  ng-if="ssstatus" class="fa fa-circle" style="color:#7FFF00"></i> {{ ssstatus ? \'ACTIVE\' : \'OFFLINE\' | translate}}',
       restrict: 'EA',
       scope: {
         status: '='
@@ -17,9 +17,11 @@ angular.module('piholeAdminApp')
       link: function postLink(scope, element, attrs) {
         var displayStatus = function(){
             if(scope.status == '1'){
-              element.html('<i class="fa fa-circle" style="color:#7FFF00"></i> Active')
+              scope.ssstatus = true;
+              //element.html('<i class="fa fa-circle" style="color:#7FFF00"></i> {{\'ACTIVE\' | translate}})')
             } else {
-              element.html('<i class="fa fa-circle" style="color:#FF0000"></i> Offline')
+              scope.ssstatus = false;
+           //   element.html('<i class="fa fa-circle" style="color:#FF0000"></i> Offline')
             }
         };
         scope.$watch('status', function (newVal, oldVal) {
