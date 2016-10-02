220b2d50c4a5dba60a19858425e27e49d7946d30
Promofaux <adzjwarner@gmail.com>
2016-10-02 02:42:54 +0100

    Tidy up whitespace

--- js/pihole/index.js
+++ js/pihole/index.js
@@ -1,12 +1,12 @@
 var globalSummary;
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Pull in data via AJAX
 
 
     updateSummaryData();
-    var check = function(){
-        if(globalSummary == null){
+    var check = function () {
+        if (globalSummary == null) {
             setTimeout(check, 50);
         }
         else {
@@ -23,9 +23,6 @@ $(document).ready(function() {
     }
 
     check();
-
-
-
 
 
     // Create charts
@@ -47,19 +44,19 @@ $(document).ready(function() {
         ]
     };
     var isMobile = {
-        Windows: function() {
+        Windows: function () {
             return /IEMobile/i.test(navigator.userAgent);
         },
-        Android: function() {
+        Android: function () {
             return /Android/i.test(navigator.userAgent);
         },
-        BlackBerry: function() {
+        BlackBerry: function () {
             return /BlackBerry/i.test(navigator.userAgent);
         },
-        iOS: function() {
+        iOS: function () {
             return /iPhone|iPad|iPod/i.test(navigator.userAgent);
         },
-        any: function() {
+        any: function () {
             return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
         }
     };
@@ -67,32 +64,31 @@ $(document).ready(function() {
     var ctx = document.getElementById("queryOverTimeChart").getContext("2d");
     timeLineChart = new Chart(ctx).Line(chartData,
         {
-            pointDot : false,
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-            animation : animate
+            pointDot: false,
+            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+            animation: animate
         }
     );
 
     ctx = document.getElementById("queryTypeChart").getContext("2d");
     queryTypeChart = new Chart(ctx).Doughnut([],
         {
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-            animation : animate
+            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+            animation: animate
         }
     );
 
     ctx = document.getElementById("forwardDestinationChart").getContext("2d");
     forwardDestinationChart = new Chart(ctx).Doughnut([],
         {
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-            animation : animate
+            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+            animation: animate
         }
     );
 });
 
 
-function addCommas(string)
-{
+function addCommas(string) {
     return string.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
 }
 
@@ -110,30 +106,31 @@ function updateSummaryData(runOnce) {
             $("h3#ads_percentage_today").addClass("glow");
         }
 
-        window.setTimeout(function(){
+        window.setTimeout(function () {
             $("h3#ads_blocked_today").text(addCommas(data['ads_blocked_today']));
             $("h3#dns_queries_today").text(addCommas(data['dns_queries_today']));
             $("h3#domains_being_blocked").text(addCommas(data['domains_being_blocked']));
             $("h3#ads_percentage_today").text((Math.round(data['ads_percentage_today'] * 10) / 10).toFixed(1) + "%");
             $("h3.statistic.glow").removeClass("glow")
         }, 500);
-    }).done(function() {
+    }).done(function () {
         if (runOnce !== true) {
             setTimeout(updateSummaryData, 10000);
 
         }
 
-    }).fail(function() {
+    }).fail(function () {
         if (runOnce !== true) {
             setTimeout(updateSummaryData, (1000 * 60 * 5));
         }
-    });;
+    });
+    ;
 }
 
 function updateQueriesOverTime() {
-    $.getJSON("api.php?overTimeData", function(data) {
+    $.getJSON("api.php?overTimeData", function (data) {
         for (hour in data["ads_over_time"]) {
-            if ($.isNumeric(hour)){ //Check value is numeric. Some strange string values were appearing, even though they're not in the raw response
+            if ($.isNumeric(hour)) { //Check value is numeric. Some strange string values were appearing, even though they're not in the raw response
                 timeLineChart.addData([data.domains_over_time[hour], data.ads_over_time[hour]], hour + ":00");
             }
         }
@@ -143,8 +140,8 @@ function updateQueriesOverTime() {
 }
 
 function updateTopClientsChart() {
-    $.getJSON("api.php?getQuerySources", function(data) {
-        var clienttable =  $('#client-frequency').find('tbody:last');
+    $.getJSON("api.php?getQuerySources", function (data) {
+        var clienttable = $('#client-frequency').find('tbody:last');
         for (domain in data.top_sources) {
             clienttable.append('<tr> <td>' + domain +
                 '</td> <td>' + data.top_sources[domain] + '</td> <td> <div class="progress progress-sm"> <div class="progress-bar progress-bar-blue" style="width: ' +
@@ -156,10 +153,12 @@ function updateTopClientsChart() {
 }
 
 function updateQueryTypes() {
-    $.getJSON("api.php?getQueryTypes", function(data) {
+    $.getJSON("api.php?getQueryTypes", function (data) {
         var colors = [];
-        $.each($.AdminLTE.options.colors, function(key, value) { colors.push(value); });
-        $.each(data, function(key , value) {
+        $.each($.AdminLTE.options.colors, function (key, value) {
+            colors.push(value);
+        });
+        $.each(data, function (key, value) {
             queryTypeChart.addData({
                 value: value,
                 color: colors.shift(),
@@ -172,9 +171,11 @@ function updateQueryTypes() {
 }
 
 function updateForwardDestinations() {
-    $.getJSON("api.php?getForwardDestinations", function(data) {
+    $.getJSON("api.php?getForwardDestinations", function (data) {
         var colors = [];
-        $.each($.AdminLTE.options.colors, function(key, value) { colors.push(value); });
+        $.each($.AdminLTE.options.colors, function (key, value) {
+            colors.push(value);
+        });
         $.each(data, function (key, value) {
             forwardDestinationChart.addData({
                 value: value,
@@ -188,7 +189,7 @@ function updateForwardDestinations() {
 }
 
 function updateTopLists() {
-    $.getJSON("api.php?topItems", function(data) {
+    $.getJSON("api.php?topItems", function (data) {
         var domaintable = $('#domain-frequency').find('tbody:last');
         var adtable = $('#ad-frequency').find('tbody:last');
 
