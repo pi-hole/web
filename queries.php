810a69956dcc1a59af13726a9dff645cde0f40d4
Adam Warner <awarner@croydon.parkeon.com>
2016-10-06 16:02:49 +0100

    FINALLY hide the columnSelection box! (By moving bootgrid options)

--- queries.php
+++ queries.php
@@ -10,53 +10,51 @@ require "header.php";
 </div>
 -->
 <div class="row">
-  <div class="col-md-12">
-    <div class="box" id="recent-queries">
-      <div class="box-header with-border">
-        <h3 class="box-title">Recent Queries</h3>
-      </div>
-      <!-- /.box-header -->
-      <div class="box-body">
-        <div class="table-responsive">
-          <table id="grid-data" class="display table table-striped table-bordered" data-toggle="bootgrid"
-                 data-ajax="true" data-url="server.php"
-          ">
-          <thead>
-          <tr>
-            <th data-column-id="ts" data-visible-in-selection="false">Time</th>
-            <th data-column-id="query_type" data-visible-in-selection="false">Type</th>
-            <th data-column-id="name" data-visible-in-selection="false">Domain</th>
-            <th data-column-id="source" data-visible-in-selection="false">Client</th>
-            <th data-column-id="piholed" data-visible-in-selection="false">Status</th>
-          </tr>
-          </thead>
-          </table>
+    <div class="col-md-12">
+        <div class="box" id="recent-queries">
+            <div class="box-header with-border">
+                <h3 class="box-title">Recent Queries</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                    <table id="grid-data" class="display table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th data-column-id="ts" data-visible-in-selection="false">Time</th>
+                            <th data-column-id="query_type" data-visible-in-selection="false">Type</th>
+                            <th data-column-id="name" data-visible-in-selection="false">Domain</th>
+                            <th data-column-id="source" data-visible-in-selection="false">Client</th>
+                            <th data-column-id="piholed" data-visible-in-selection="false">Status</th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+            <!-- /.box-body -->
         </div>
-      </div>
-      <!-- /.box-body -->
+        <!-- /.box -->
     </div>
-    <!-- /.box -->
-  </div>
 </div>
 
 <table id="hosts" hidden="true">
-  <thead>
-  <tr>
-    <th>IP</th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php //Read in /etc/hosts and show in this hidden table for later use
-  $hosts = file_exists("/etc/hosts") ? file("/etc/hosts") : array();
-  foreach ($hosts as $host){
-    $x = preg_split('/\s+/', $host);
-    if ($x[0] != '' && $x[0] != '#'){
-      echo '<tr><td>'.$x[0].'#'.$x[1].'</td></tr>';
-    }
+    <thead>
+    <tr>
+        <th>IP</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php //Read in /etc/hosts and show in this hidden table for later use
+    $hosts = file_exists("/etc/hosts") ? file("/etc/hosts") : array();
+    foreach ($hosts as $host){
+        $x = preg_split('/\s+/', $host);
+        if ($x[0] != '' && $x[0] != '#'){
+            echo '<tr><td>'.$x[0].'#'.$x[1].'</td></tr>';
+        }
 
-  }
-  ?>
-  </tbody>
+    }
+    ?>
+    </tbody>
 </table>
 
 
@@ -67,34 +65,40 @@ require "footer.php";
 ?>
 
 <script language="javascript">
-  //Refer to http://jquery-bootgrid.com/Documentation for methods, events and settings
-  //load grid on page\e load...
-  $("#grid-data").bootgrid().on("loaded.rs.jquery.bootgrid", function (e) { //once table has loaded, set the colours for easy recognition of blocked queries.
+    //Refer to http://jquery-bootgrid.com/Documentation for methods, events and settings
+    //load grid on page\e load...
+    $("#grid-data").bootgrid({
+        ajax:true,
+        url:"server.php",
+        columnSelection:false,
+        rowCount: [10,25,50,100]
 
-    $("table tr td:nth-child(5)").each(function () {//Status column
-      if ($(this).text() == '0') {
-        $(this).html('OK!');
-        $(this).parent().css('color', 'green');
-      }
-      else {
-        $(this).html('Pi-holed');
-        $(this).parent().css('color', 'red');
-      }
-    });
-    //Remember the hidden table from earlier? This is where we apply the host name to the Client column!
-    $("table tr td:nth-child(4)").each(function () {//Client column
-      var str=$(this).text(); // Save text out to a variable
-      $("#hosts").find("tr td").each(function(){//loop through hidden table rows
-        var comp = $(this).text();
-        var arr = comp.split('#'); //split each row on #
-        if (arr[0] == str){
-          str=(arr[1] +" ("+str+")"); //if IP address matches, then we have a hostname!
-        }
-      });
-      $(this).html(str); //set the cell's text to the new hostname(ip)
-    });
+    }).on("loaded.rs.jquery.bootgrid", function (e) { //once table has loaded, set the colours for easy recognition of blocked queries.
 
-  });
+        $("table tr td:nth-child(5)").each(function () {//Status column
+            if ($(this).text() == '0') {
+                $(this).html('OK!');
+                $(this).parent().css('color', 'green');
+            }
+            else {
+                $(this).html('Pi-holed');
+                $(this).parent().css('color', 'red');
+            }
+        });
+        //Remember the hidden table from earlier? This is where we apply the host name to the Client column!
+        $("table tr td:nth-child(4)").each(function () {//Client column
+            var str=$(this).text(); // Save text out to a variable
+            $("#hosts").find("tr td").each(function(){//loop through hidden table rows
+                var comp = $(this).text();
+                var arr = comp.split('#'); //split each row on #
+                if (arr[0] == str){
+                    str=(arr[1] +" ("+str+")"); //if IP address matches, then we have a hostname!
+                }
+            });
+            $(this).html(str); //set the cell's text to the new hostname(ip)
+        });
+
+    });
 
 
 </script>
