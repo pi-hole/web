77e072493eda0f2cb54d55de7aa1d51d315c39f2
DL6ER <derigs@ph1.uni-koeln.de>
2016-11-19 13:04:51 +0100

    Added comments in index.php

--- index.php
+++ index.php
@@ -76,7 +76,12 @@
       </div>
     </div>
 </div>
-<?php if($auth){ ?>
+<?php
+  // If the user is logged in, then we show the more detailed index page.
+  // Even if we would include them here anyhow, there would be nothing to
+  // show since the API will respect the privacy of the user if he defines
+  // a password
+  if($auth){ ?>
 <div class="row">
     <div class="col-md-6">
     <div class="box" id="query-types">
