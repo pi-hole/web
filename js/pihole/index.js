e94a2be01664f4effdf06e0e581db5287ceda90b
DL6ER <derigs@ph1.uni-koeln.de>
2016-11-21 18:58:04 +0100

    More codacy complaints

--- js/pihole/index.js
+++ js/pihole/index.js
@@ -261,9 +261,9 @@ function updateTopLists() {
     $.getJSON("api.php?summaryRaw&topItems", function(data) {
         var domaintable = $('#domain-frequency').find('tbody:last');
         var adtable = $('#ad-frequency').find('tbody:last');
-        var url;
+        var url, domain;
 
-        for (var domain in data.top_queries) {
+        for (domain in data.top_queries) {
             if(domain !== "pi.hole")
             {
                 url = "<a href=\"queries.php?domain="+domain+"\">"+domain+"</a>";
@@ -276,7 +276,7 @@ function updateTopLists() {
                 '</td> <td>' + data.top_queries[domain] + '</td> <td> <div class="progress progress-sm"> <div class="progress-bar progress-bar-green" style="width: ' +
                 data.top_queries[domain] / data.dns_queries_today * 100 + '%"></div> </div> </td> </tr> ');
         }
-        for (var domain in data.top_ads) {
+        for (domain in data.top_ads) {
             url = "<a href=\"queries.php?domain="+domain+"\">"+domain+"</a>";
             adtable.append("<tr> <td>" + url +
                 '</td> <td>' + data.top_ads[domain] + '</td> <td> <div class="progress progress-sm"> <div class="progress-bar progress-bar-yellow" style="width: ' +
