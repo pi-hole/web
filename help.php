0680fd546bf87f2a85b80e5a2bef3a4d5e8c7159
DL6ER <derigs@ph1.uni-koeln.de>
2016-11-22 12:43:35 +0100

    Added help center skeleton

---
+++ help.php
@@ -0,0 +1,60 @@
+<?php
+    require "header.php";
+?>
+<!-- Title -->
+<div class="page-header">
+    <h1>Help center</h1>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Header</h2>
+    <h3>Top left: Status display</h3>
+    <h3>Top right: About</h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Main page</h2>
+    <p>On the main page, various statistics of pi-hole are shown to the user.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Query Log</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>White- / Blacklist</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Update lists</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Disable / Enable</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Donate</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Help (this page)</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <h2>Footer</h2>
+    </div>
+</div>
+
+
+<?php
+    require "footer.php";
+?>
