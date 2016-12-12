281fe28388b7df064b1cb3e1b83d190c99b87f97
DL6ER <derigs@ph1.uni-koeln.de>
2016-12-12 11:01:58 +0100

    Updated start button

--- webupdate.php
+++ webupdate.php
@@ -6,7 +6,7 @@
     <h1>Pi-hole web updater</h1>
 </div>
 
-<button id="btnStart" class="btn btn-default" type="button">Start update</button>
+<button id="btnStart" class="btn btn-lg btn-primary btn-block" type="button">Start update</button>
 
 
 <pre id="output" style="width: 100%; height: 100%;"></pre>
