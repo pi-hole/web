721e283a6759f26a23fab2deb6acc711a9513c71
DL6ER <derigs@ph1.uni-koeln.de>
2016-12-13 11:55:10 +0100

    Minor change

--- php/loginpage.php
+++ php/loginpage.php
@@ -4,10 +4,10 @@
     <img src="img/lock.svg" width="60%">
     <div class="page-header">Protected page</div>
     <form action="" method="POST">
-        <div class="form-group <?php if($wrongpassword){ ?>has-error<?php } ?> input-lg">
+        <div class="form-group <?php if($wrongpassword){ ?>has-error<?php } ?> input-group-lg">
         <?php if($wrongpassword){ ?><label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Wrong password!</label><?php } ?>
         <div class="input-group">
-            <input type="password" class="form-control " name="pw" placeholder="Enter password to continue">
+            <input type="password" class="form-control" name="pw" placeholder="Enter password to continue">
             <span class="input-group-btn">
               <button type="submit" class="btn btn-info btn-flat <?php if($wrongpassword){ ?>btn-danger<?php } ?>">Submit</button>
           </span>
