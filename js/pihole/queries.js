bf613599aa6860937316d1189a73602f88fe7769
Adam Warner <awarner@croydon.parkeon.com>
2016-12-16 10:46:07 +0000

    https://www.codacy.com/app/Pi-hole/AdminLTE/file/4753758894/issues/source?bid=3919163&fileBranchId=3919163#l49

--- js/pihole/queries.js
+++ js/pihole/queries.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
     });
     $("#all-queries tbody").on( "click", "button", function () {
         var data = tableApi.row( $(this).parents("tr") ).data();
-        if (data[4] == "Pi-holed")
+        if (data[4] === "Pi-holed")
         {
           add(data[2],"white");
         }
