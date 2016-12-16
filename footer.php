822c8cf1c8e6a68c0afa04a82dcd4809ad08192a
DL6ER <derigs@ph1.uni-koeln.de>
2016-12-16 22:58:28 +0100

    Fix the footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
-    <footer class="main-footer row">
+    <footer class="main-footer">
         <i class="fa fa-github"></i> <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY">Donate</a></strong> if you found this useful.
         <div class="pull-right hidden-xs">
             <?php
@@ -28,7 +28,6 @@
             }
             ?>
             <b>Pi-hole Version </b> <span id="piholeVersion"><?php echo $piholeVersion; ?></span><?php if(isset($piholeCommit)) { echo " (".$piholeBranch.", ".$piholeCommit.")"; } ?>
-            <?php if($piholeBranch !== "master" || $webBranch !== "master") { ?><br/><?php } ?>
             <b>Web Interface Version </b> <span id="webVersion"><?php echo $webVersion; ?></span><?php if(isset($webCommit)) { echo " (".$webBranch.", ".$webCommit.")"; } ?>
         </div>
     </footer>
