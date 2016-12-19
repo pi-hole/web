10ef50a6eb0fdf94d2a41b08cd186970a19a94d4
DL6ER <derigs@ph1.uni-koeln.de>
2016-12-19 11:59:09 +0100

    Removed another $hostname = ...

--- data.php
+++ data.php
@@ -307,7 +307,6 @@
     }
 
     function countBlockedQueries() {
-        $hostname = trim(file_get_contents("/etc/hostname"), "\x00..\x1F");
         return exec("grep \"gravity.list\" /var/log/pihole.log | grep -c \" is \"");
     }
 
