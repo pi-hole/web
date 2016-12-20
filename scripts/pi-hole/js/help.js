fab94e07fdcd0f54fede4eca084e9e122f1fcb02
Mcat12 <newtoncat12@yahoo.com>
2016-12-19 19:59:08 -0500

    Fix php links in js files
    Also removed help.js because it's no longer used

--- scripts/pi-hole/js/help.js
+++
@@ -1,7 +0,0 @@
-$( "#flush" ).click(function() {
-    if (confirm("Are you sure you want to flush the Pi-hole log file?")) {
-        document.location.href="../../help.php";
-    } else {
-        // Do nothing!
-    }
-});
