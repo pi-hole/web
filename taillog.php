a47eb094dd88e77f9b521d9dd8aed16d6c60c8e0
DL6ER <dl6er@dl6er.de>
2016-12-23 13:53:18 +0100

    Add auto scrolling feature

--- taillog.php
+++ taillog.php
@@ -6,7 +6,9 @@
     <h1>Output the last lines of the pihole.log file (live)</h1>
 </div>
 
+<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk1"> enable automatic scrolling on update</label></div>
 <pre id="output" style="width: 100%; height: 100%;"></pre>
+<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk2"> enable automatic scrolling on update</label></div>
 
 <?php
     require "footer.php";
