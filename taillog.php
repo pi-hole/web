05fba3e54275461e2afef5c7d4053540037e7ca8
DL6ER <dl6er@dl6er.de>
2016-12-23 13:55:29 +0100

    Adjusted wording slightly

--- taillog.php
+++ taillog.php
@@ -6,9 +6,9 @@
     <h1>Output the last lines of the pihole.log file (live)</h1>
 </div>
 
-<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk1"> enable automatic scrolling on update</label></div>
+<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk1"> Automatic scrolling on update</label></div>
 <pre id="output" style="width: 100%; height: 100%;"></pre>
-<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk2"> enable automatic scrolling on update</label></div>
+<div class="checkbox"><label><input type="checkbox" name="active" checked id="chk2"> Automatic scrolling on update</label></div>
 
 <?php
     require "footer.php";
