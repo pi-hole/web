dfa173754288d9226b3a7825a621380c40847f1f
DL6ER <dl6er@dl6er.de>
2016-12-25 20:00:24 +0100

    Be ablw to specify how many Top Items a user wants to see through the API call

--- data.php
+++ data.php
@@ -66,7 +66,7 @@
         );
     }
 
-    function getTopItems($qty=10) {
+    function getTopItems($argument) {
         global $log,$setupVars;
 
         // Process log file
@@ -86,6 +86,14 @@
         // Prepare arrays for Top Items
         $topDomains = []; $domaincounter = 0;
         $topAds = []; $adcounter = 0;
+
+        // Default number of Top Items to show is 10
+        $qty = 10;
+
+        if(is_numeric($argument))
+        {
+            $qty = intval($argument);
+        }
 
         foreach ($dns_domains as $key => $value) {
             if(isset($gravity_domains[$key]) && $adcounter < $qty)
