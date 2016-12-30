5ab9f75e45aad61d3d98d1018e9ba9c7d514a41b
Mcat12 <newtoncat12@yahoo.com>
2016-12-30 11:49:39 -0500

    Condense list verification and use in both add and sub

--- php/add.php
+++ php/add.php
@@ -1,35 +1,12 @@
 <?php
 require('auth.php');
 
-if(!isset($_POST['domain'], $_POST['list']) && !(isset($_POST['pw']) || isset($_POST['token']))) {
-	log_and_die("Missing POST variables");
-}
+$type = $_POST['list'];
 
-if(isset($_POST['token']))
-{
-	check_cors();
-	check_csrf($_POST['token']);
-}
-elseif(isset($_POST['pw']) && $_POST['list'] == "white")
-{
-	require("password.php");
-	if(strlen($pwhash) == 0)
-	{
-		log_and_die("No password set - whitelisting with password not supported");
-	}
-	elseif($wrongpassword)
-	{
-		log_and_die("Wrong password - whitelisting of ${_POST['domain']} not permitted");
-	}
-}
-else
-{
-	log_and_die("Not allowed!");
-}
-check_domain();
+// All of the verification for list editing
+list_verify($type);
 
-
-switch($_POST['list']) {
+switch($type) {
 	case "white":
 		echo exec("sudo pihole -w -q ${_POST['domain']}");
 		break;
