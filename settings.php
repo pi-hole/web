8414ddf115f072aac62c20bd09605bed4b2d37b2
DL6ER <dl6er@dl6er.de>
2016-12-31 15:46:47 +0000

    Changed wording

--- settings.php
+++ settings.php
@@ -542,7 +542,7 @@
 			<form role="form" method="post">
 			<div class="box-body">
 				<div class="col-lg-12">
-				<label>Lists maintained by the Pi-hole team (reset on updates)</label>
+				<label>Lists suggested by the Pi-hole team (reset on updates)</label>
 					<?php foreach ($adlistsdefault as $key => $value) { ?>
 					<div class="form-group">
 						<div class="checkbox"><label style="word-break: break-word;"><input type="checkbox" name="adlist-<?php echo $key; ?>" <?php if($value[0]){ ?>checked<?php } ?>> <a href="<?php echo htmlentities ($value[1]); ?>" target="_new"><?php echo htmlentities($value[1]); ?></a></label></div>
