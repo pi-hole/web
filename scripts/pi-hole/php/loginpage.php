ed014345061cfc59eb446c2b298a55e65bb63689
DL6ER <dl6er@dl6er.de>
2017-01-02 13:24:48 +0100

    Strg+Return -> Go immediately to Settings page

--- scripts/pi-hole/php/loginpage.php
+++ scripts/pi-hole/php/loginpage.php
@@ -13,9 +13,9 @@
     </div>
 
     <div class="panel-body">
-      <form action="" method="post">
+      <form action="" id="loginform" method="post">
         <div class="form-group has-feedback <?php if ($wrongpassword) { ?>has-error<?php } ?> ">
-          <input type="password" name="pw" class="form-control" placeholder="Password" autofocus>
+          <input type="password" id="loginpw" name="pw" class="form-control" placeholder="Password" autofocus>
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
