b41e4e0a0ae6e43eede6d9d19b158540a17286af
DL6ER <dl6er@dl6er.de>
2017-01-02 16:29:53 +0100

    Add infotext

--- scripts/pi-hole/php/loginpage.php
+++ scripts/pi-hole/php/loginpage.php
@@ -19,7 +19,13 @@
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
-          <div class="col-xs-4 col-xs-offset-8">
+          <div class="col-xs-8">
+          <ul>
+            <li>Return &rarr; Log in and go to Main page</li>
+            <li>Ctrl+Return &rarr; Log in and go to Settings page</li>
+          </ul>
+          </div>
+          <div class="col-xs-4">
             <button type="submit" href="#" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-log-in"></i>&nbsp;&nbsp;&nbsp;Log in</button>
           </div>
         </div>
