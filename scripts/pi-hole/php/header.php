ae9a2ad6167aa341a34f8a80aa9fc81a11783ce9
DL6ER <dl6er@dl6er.de>
2017-01-03 10:57:39 +0100

    Fix responsive icon on Gecko engine (Firefox + derivatives)

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -249,7 +249,7 @@
             <!-- Sidebar user panel -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="img/logo.svg" class="img-responsive" alt="Pi-hole logo" />
+                    <img src="img/logo.svg" class="img-responsive" alt="Pi-hole logo" style="display: table; table-layout: fixed; height: 67px;" />
                 </div>
                 <div class="pull-left info">
                     <p>Status</p>
