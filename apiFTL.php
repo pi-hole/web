7c6254f3e793b7eebfa8d555a421fed22f21cbf9
DL6ER <dl6er@dl6er.de>
2017-01-06 14:35:43 +0100

    Implemented "overTimeData10mins"

--- apiFTL.php
+++ apiFTL.php
@@ -1,10 +1,10 @@
 <?php
 	error_reporting(E_ALL);
 	require("scripts/pi-hole/php/FTL.php");
-	$socket = connectFTL("127.0.0.1", 4712);
+	$socket = connectFTL("127.0.0.1", 4711);
 	header('Content-type: application/json');
 
-
+	$data = [];
 
 	if (isset($_GET['summary']) || isset($_GET['summaryRaw'])) {
 		sendRequestFTL("stats");
@@ -13,7 +13,7 @@
 		$stats = [];
 		foreach($return as $line)
 		{
-			$tmp = explode(": ",$line);
+			$tmp = explode(" ",$line);
 
 			if(isset($_GET['summary']))
 			{
@@ -31,10 +31,27 @@
 				$stats[$tmp[0]] = $tmp[1];
 			}
 		}
-		echo json_encode($stats);
+		$data = array_merge($data,$stats);
 	}
 
+	if (isset($_GET['overTimeData10mins'])) {
+		sendRequestFTL("overTime");
+		$return = getResponseFTL();
 
+		$domains_over_time = [];
+		$ads_over_time = [];
+		foreach($return as $line)
+		{
+			$tmp = explode(" ",$line);
+			$domains_over_time[] = $tmp[1];
+			$ads_over_time[] = $tmp[2];
+		}
+		$result = ['domains_over_time' => $domains_over_time,
+		           'ads_over_time' => $ads_over_time];
+		$data = array_merge($data, $result);
+	}
+
+	echo json_encode($data);
 
 	disconnectFTL();
 
