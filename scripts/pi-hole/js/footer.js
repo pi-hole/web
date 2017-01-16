f840caf17385fd0fa65f8fe2b3aff8e3bac38028
DL6ER <dl6er@dl6er.de>
2017-01-16 16:32:29 +0100

    codacy...

--- scripts/pi-hole/js/footer.js
+++ scripts/pi-hole/js/footer.js
@@ -207,11 +207,14 @@ $(document).keypress(function(e) {
 
 function testCookies()
 {
-    if (navigator.cookieEnabled) return true;
+    if (navigator.cookieEnabled)
+    {
+        return true;
+    }
 
     // set and read cookie
     document.cookie = "cookietest=1";
-    var ret = document.cookie.indexOf("cookietest=") != -1;
+    var ret = document.cookie.indexOf("cookietest=") !== -1;
 
     // delete cookie
     document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
