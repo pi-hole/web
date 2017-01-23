ea23d77bd30b71a32e430e5fa246a92c52f325c0
DL6ER <dl6er@dl6er.de>
2017-01-23 14:49:27 +0100

    Reduce workload in updater process

--- scripts/pi-hole/js/webupdate.js
+++ scripts/pi-hole/js/webupdate.js
@@ -1,8 +1,7 @@
 function getContent() {
     $.ajax({
         url: "scripts/pi-hole/php/webupdater.php?getlog",
-        success(result) { $("#output").text(result); },
-        complete() { setTimeout( getContent(), 250); }
+        success(result) { $("#output").text(result); }
     });
 }
 
@@ -12,7 +11,7 @@ function startupdate() {
     // First wait 0.5 sec so that the update can be started
     // and we can be sure that the log file has been flushed
     // then update the display every 100ms
-    window.setTimeout(function(){ getContent(); }, 500);
+    setTimeout(function(){ setInterval(function(){ getContent(); }, 100); }, 500);
 
     return false;
 }
