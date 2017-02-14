e5d4ca328a42fe8503b3dd848b68c66bf8f4a51b
DL6ER <dl6er@dl6er.de>
2017-02-14 15:01:40 +0100

    Added header to scripts/pi-hole/js files

--- scripts/pi-hole/js/footer.js
+++ scripts/pi-hole/js/footer.js
@@ -1,3 +1,9 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
 // User menu toggle
 $("#dropdown-menu a").on("click", function(event) {
     $(this).parent().toggleClass("open");
