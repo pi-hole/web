1c73016f6a4bee9e546a9fb2cea43c47c950401b
DL6ER <dl6er@dl6er.de>
2017-02-14 15:00:11 +0100

    Modified scripts in scripts/pi-hole/php

--- scripts/pi-hole/php/data.php
+++ scripts/pi-hole/php/data.php
@@ -1,4 +1,12 @@
 <?php
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */ ?>
+
+<?php
     $log = array();
     $setupVars = parse_ini_file("/etc/pihole/setupVars.conf");
 
