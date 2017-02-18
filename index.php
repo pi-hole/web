4f70973cbce02720513e03f991046ef4ff43905b
DL6ER <DL6ER@users.noreply.github.com>
2017-02-18 14:20:51 +0100

    EUPL license (#402)
    * Move whole project to EUPL, copy MIT license to scripts/vendor/ and style/vendor/
    
    * Added header to main PHP files
    
    * Modified scripts in scripts/pi-hole/php
    
    * Added header to scripts/pi-hole/js files
    
    * Added license header to our custom style script
    
    * Slight reformulation

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!-- Pi-hole: A black hole for Internet advertisements
+*    (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*    Network-wide ad blocking via your own hardware.
+*
+*    This file is copyright under the latest version of the EUPL.
+*    Please see LICENSE file for your rights under this license. -->
 <?php
     $indexpage = true;
     require "scripts/pi-hole/php/header.php";
--- scripts/pi-hole/js/taillog.js
+++ scripts/pi-hole/js/taillog.js
@@ -1,3 +1,9 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
 var offset, timer, pre, scrolling = true;
 
 // Check every 200msec for fresh data
