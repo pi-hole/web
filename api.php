d7fdaf9273ad0a3f352bc62d8532330c56b915aa
DL6ER <dl6er@dl6er.de>
2017-02-19 21:19:02 +0100

    Fix whitespace caused by added license headers

--- api.php
+++ api.php
@@ -1,10 +1,11 @@
-<!-- Pi-hole: A black hole for Internet advertisements
+<?php
+/* Pi-hole: A black hole for Internet advertisements
 *    (c) 2017 Pi-hole, LLC (https://pi-hole.net)
 *    Network-wide ad blocking via your own hardware.
 *
 *    This file is copyright under the latest version of the EUPL.
-*    Please see LICENSE file for your rights under this license. -->
-<?php
+*    Please see LICENSE file for your rights under this license. */
+
     $api = true;
     require "scripts/pi-hole/php/password.php";
     require "scripts/pi-hole/php/auth.php";
