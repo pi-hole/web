9c118622356a1c69949860dbd3d1e4a9fe7e43d7
DL6ER <dl6er@dl6er.de>
2017-02-20 12:36:04 +0100

    Pass only token

--- scripts/pi-hole/php/api_token.php
+++ scripts/pi-hole/php/api_token.php
@@ -4,11 +4,22 @@ require "auth.php";
 require "password.php";
 check_cors();
 
-if($auth && (strlen($pwhash) > 0))
+if($auth)
 {
-  require_once("../../vendor/qrcode.php");
-  $qr = QRCode::getMinimumQRCode("WEBPASSWORD=$pwhash", QR_ERROR_CORRECT_LEVEL_Q);
-  $qr->printHTML("10px");
+  if(strlen($pwhash) > 0)
+  {
+    require_once("../../vendor/qrcode.php");
+    $qr = QRCode::getMinimumQRCode($pwhash, QR_ERROR_CORRECT_LEVEL_Q);
+    $qr->printHTML("10px");
+  }
+  else
+  {
+  ?><p>No password set<?php
+  }
+}
+else
+{
+?><p>Not authorized!</p><?php
 }
 ?>
 </body>
