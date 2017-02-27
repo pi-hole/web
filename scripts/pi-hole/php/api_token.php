d104539708a03ecd201b0a5f3e1deb57a467cc2e
DL6ER <DL6ER@users.noreply.github.com>
2017-02-27 22:33:35 +0100

    API QR token generator (#410)
    * Added API QR token generator
    
    * Pass only token
    
    * Corrected link

---
+++ scripts/pi-hole/php/api_token.php
@@ -0,0 +1,26 @@
+<html><body>
+<?php
+require "auth.php";
+require "password.php";
+check_cors();
+
+if($auth)
+{
+  if(strlen($pwhash) > 0)
+  {
+    require_once("../../vendor/qrcode.php");
+    $qr = QRCode::getMinimumQRCode($pwhash, QR_ERROR_CORRECT_LEVEL_Q);
+    $qr->printHTML("10px");
+  }
+  else
+  {
+  ?><p>No password set<?php
+  }
+}
+else
+{
+?><p>Not authorized!</p><?php
+}
+?>
+</body>
+</html>
