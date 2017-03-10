pi-hole/web

commit 276f2c1628ffef72c3d3ae489fd5b71ebb800227

Allow usage of Teleporter and QueryAds backend only if a valid session is detected (#415)

scripts/pi-hole/php/queryads.php
@@ -6,6 +6,9 @@
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
+require "password.php";
+if(!$auth) die("Not authorized");
+
 ob_end_flush();
 ini_set("output_buffering", "0");
 ob_implicit_flush(true);

scripts/pi-hole/php/teleporter.php
@@ -4,9 +4,11 @@
 *  Network-wide ad blocking via your own hardware.
 *
 *  This file is copyright under the latest version of the EUPL.
-*  Please see LICENSE file for your rights under this license. */ ?>
+*  Please see LICENSE file for your rights under this license. */
 
-<?php
+require "password.php";
+if(!$auth) die("Not authorized");
+
 require('func.php');
 function process_zip($name)
 {

commit 667acb90caef44e7b847f19152eae2169b9b22fe

Allow block page to access queryads
Broken in https://github.com/pi-hole/AdminLTE/commit/276f2c1628ffef72c3d3ae489fd5b71ebb800227

scripts/pi-hole/php/queryads.php
@@ -6,9 +6,6 @@
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
-require "password.php";
-if(!$auth) die("Not authorized");
-
 ob_end_flush();
 ini_set("output_buffering", "0");
 ob_implicit_flush(true);