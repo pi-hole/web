056a2a7a917a16789cb0671757b325c9e2f4670b
dovecode <nicolai@dufva.net>
2017-05-01 21:24:31 -0400

    Replaced tabs with spaces and added comments to header.php

--- scripts/pi-hole/php/settings_webui.php
+++ scripts/pi-hole/php/settings_webui.php
@@ -1,51 +1,51 @@
 <?php
-	// CPU temperature unit
-	if(isset($setupVars["TEMPERATUREUNIT"]))
-	{
-		$temperatureunit = $setupVars["TEMPERATUREUNIT"];
-	}
-	else
-	{
-		$temperatureunit = "C";
-	}
-	// Use $boxedlayout value determined in header.php
+    // CPU temperature unit
+    if(isset($setupVars["TEMPERATUREUNIT"]))
+    {
+        $temperatureunit = $setupVars["TEMPERATUREUNIT"];
+    }
+    else
+    {
+        $temperatureunit = "C";
+    }
+    // Use $boxedlayout value determined in header.php
 ?>
-		<div class="box box-success">
-			<div class="box-header with-border">
-				<h3 class="box-title">Web User Interface</h3>
-			</div>
-			<form role="form" method="post">
-			<div class="box-body">
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title">Web User Interface</h3>
+            </div>
+            <form role="form" method="post">
+            <div class="box-body">
 <?php /*
-				<h4>Query Log Page</h4>
-				<div class="col-lg-6">
-					<div class="form-group">
-						<label>Default value for <em>Show XX entries</em></label>
-						<select class="form-control" disabled>
-							<option>10</option>
-							<option>25</option>
-							<option>50</option>
-							<option>100</option>
-							<option>All</option>
-						</select>
-					</div>
-				</div>
+                <h4>Query Log Page</h4>
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label>Default value for <em>Show XX entries</em></label>
+                        <select class="form-control" disabled>
+                            <option>10</option>
+                            <option>25</option>
+                            <option>50</option>
+                            <option>100</option>
+                            <option>All</option>
+                        </select>
+                    </div>
+                </div>
 */ ?>
-				<h4>Interface appearance</h4>
-				<div class="form-group">
-					<div class="checkbox"><label><input type="checkbox" name="boxedlayout" value="yes" <?php if($boxedlayout){ ?>checked<?php } ?> >Use boxed layout (helpful when working on large screens)</label></div>
-				</div>
-				<h4>CPU Temperature Unit</h4>
-				<div class="form-group">
-					<div class="radio"><label><input type="radio" name="tempunit" value="C" <?php if($temperatureunit === "C"){ ?>checked<?php } ?> >Celsius</label></div>
-					<div class="radio"><label><input type="radio" name="tempunit" value="K" <?php if($temperatureunit === "K"){ ?>checked<?php } ?> >Kelvin</label></div>
-					<div class="radio"><label><input type="radio" name="tempunit" value="F" <?php if($temperatureunit === "F"){ ?>checked<?php } ?> >Fahrenheit</label></div>
-				</div>
-			</div>
-			<div class="box-footer">
-				<input type="hidden" name="field" value="webUI">
-				<input type="hidden" name="token" value="<?php echo $token ?>">
-				<button type="submit" class="btn btn-primary pull-right">Save</button>
-			</div>
-			</form>
-		</div>
+                <h4>Interface appearance</h4>
+                <div class="form-group">
+                    <div class="checkbox"><label><input type="checkbox" name="boxedlayout" value="yes" <?php if($boxedlayout){ ?>checked<?php } ?> >Use boxed layout (helpful when working on large screens)</label></div>
+                </div>
+                <h4>CPU Temperature Unit</h4>
+                <div class="form-group">
+                    <div class="radio"><label><input type="radio" name="tempunit" value="C" <?php if($temperatureunit === "C"){ ?>checked<?php } ?> >Celsius</label></div>
+                    <div class="radio"><label><input type="radio" name="tempunit" value="K" <?php if($temperatureunit === "K"){ ?>checked<?php } ?> >Kelvin</label></div>
+                    <div class="radio"><label><input type="radio" name="tempunit" value="F" <?php if($temperatureunit === "F"){ ?>checked<?php } ?> >Fahrenheit</label></div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <input type="hidden" name="field" value="webUI">
+                <input type="hidden" name="token" value="<?php echo $token ?>">
+                <button type="submit" class="btn btn-primary pull-right">Save</button>
+            </div>
+            </form>
+        </div>
--- settings.php
+++ settings.php
@@ -5,32 +5,32 @@
 *
 *    This file is copyright under the latest version of the EUPL.
 *    Please see LICENSE file for your rights under this license. */
-	require "scripts/pi-hole/php/header.php";
-	require "scripts/pi-hole/php/savesettings.php";
-	// Reread ini file as things might have been changed
-	$setupVars = parse_ini_file("/etc/pihole/setupVars.conf");
+    require "scripts/pi-hole/php/header.php";
+    require "scripts/pi-hole/php/savesettings.php";
+    // Reread ini file as things might have been changed
+    $setupVars = parse_ini_file("/etc/pihole/setupVars.conf");
 ?>
 <style type="text/css">
-	.tooltip-inner {
-		max-width: none;
-		white-space: nowrap;
-	}
-	@-webkit-keyframes Pulse{
-		from {color:#630030;-webkit-text-shadow:0 0 9px #333;}
-		50% {color:#e33100;-webkit-text-shadow:0 0 18px #e33100;}
-		to {color:#630030;-webkit-text-shadow:0 0 9px #333;}
-	}
-	p.lookatme {
-		-webkit-animation-name: Pulse;
-		-webkit-animation-duration: 2s;
-		-webkit-animation-iteration-count: infinite;
-	}
+    .tooltip-inner {
+        max-width: none;
+        white-space: nowrap;
+    }
+    @-webkit-keyframes Pulse{
+        from {color:#630030;-webkit-text-shadow:0 0 9px #333;}
+        50% {color:#e33100;-webkit-text-shadow:0 0 18px #e33100;}
+        to {color:#630030;-webkit-text-shadow:0 0 9px #333;}
+    }
+    p.lookatme {
+        -webkit-animation-name: Pulse;
+        -webkit-animation-duration: 2s;
+        -webkit-animation-iteration-count: infinite;
+    }
 </style>
 
 <?php // Check if ad lists should be updated after saving ...
 if(isset($_POST["submit"])) {
-	if($_POST["submit"] == "saveupdate") {
-	// If that is the case -> refresh to the gravity page and start updating immediately
+    if($_POST["submit"] == "saveupdate") {
+    // If that is the case -> refresh to the gravity page and start updating immediately
 ?>
 <meta http-equiv="refresh" content="1;url=gravity.php?go">
 <?php }} ?>
@@ -60,93 +60,93 @@ if(isset($_POST["submit"])) {
 <?php } ?>
 
 <?php
-	$includeWidget = "";
+    $includeWidget = "";
 
-	$settingsWidget = $_GET["w"];
-	
-	$settingsWidgets = array(
-		"networking"	=> "scripts/pi-hole/php/settings_networking.php",
-		"dhcp"			=> "scripts/pi-hole/php/settings_dhcp.php",
-		"upstream_dns"	=> "scripts/pi-hole/php/settings_upstream_dns.php",
-		"querylogging"	=> "scripts/pi-hole/php/settings_querylogging.php",
-		"blocklist"		=> "scripts/pi-hole/php/settings_blocklist.php",
-		"api"			=> "scripts/pi-hole/php/settings_api.php",
-		"webui"			=> "scripts/pi-hole/php/settings_webui.php",
-		"sysadmin"		=> "scripts/pi-hole/php/settings_sysadmin.php",
-		"ftl"			=> "scripts/pi-hole/php/settings_ftl.php",
-		"teleporter"	=> "scripts/pi-hole/php/settings_teleporter.php",
-	);
-	
-	$includeWidget = $settingsWidgets[$settingsWidget];
-	
+    $settingsWidget = $_GET["w"];
+    
+    $settingsWidgets = array(
+        "networking"    => "scripts/pi-hole/php/settings_networking.php",
+        "dhcp"          => "scripts/pi-hole/php/settings_dhcp.php",
+        "upstream_dns"  => "scripts/pi-hole/php/settings_upstream_dns.php",
+        "querylogging"  => "scripts/pi-hole/php/settings_querylogging.php",
+        "blocklist"     => "scripts/pi-hole/php/settings_blocklist.php",
+        "api"           => "scripts/pi-hole/php/settings_api.php",
+        "webui"         => "scripts/pi-hole/php/settings_webui.php",
+        "sysadmin"      => "scripts/pi-hole/php/settings_sysadmin.php",
+        "ftl"           => "scripts/pi-hole/php/settings_ftl.php",
+        "teleporter"    => "scripts/pi-hole/php/settings_teleporter.php",
+    );
+    
+    $includeWidget = $settingsWidgets[$settingsWidget];
+    
 ?>
 <div class="row">
-	
-	<?php
-		if (!empty($includeWidget)) {
-			/* 
-			 * Tell included widget that it can expand sections
-			 */
-			$collapse = FALSE;
-	?>
+    
+    <?php
+        if (!empty($includeWidget)) {
+            /* 
+             * Tell included widget that it can expand sections
+             */
+            $collapse = FALSE;
+    ?>
     <div class="col-md-12">
-		<?php require $includeWidget; ?>
-	</div>
-	<?php
-		}
-		else {
-			/* 
-			 * Tell included widget to not expand sections
-			 */
-			$collapse = TRUE;
-	?>
-	<div class="col-md-6">
-		
-		<?php require 'scripts/pi-hole/php/settings_networking.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_dhcp.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_upstream_dns.php' ?>
+        <?php require $includeWidget; ?>
+    </div>
+    <?php
+        }
+        else {
+            /* 
+             * Tell included widget to not expand sections
+             */
+            $collapse = TRUE;
+    ?>
+    <div class="col-md-6">
+        
+        <?php require 'scripts/pi-hole/php/settings_networking.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_dhcp.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_upstream_dns.php' ?>
 
-	</div>
-	<div class="col-md-6">
-		
-		<?php require 'scripts/pi-hole/php/settings_querylogging.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_blocklist.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_api.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_webui.php' ?>
+    </div>
+    <div class="col-md-6">
+        
+        <?php require 'scripts/pi-hole/php/settings_querylogging.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_blocklist.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_api.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_webui.php' ?>
 
-<?php /*
-		<div class="box box-info">
-			<div class="box-header with-border">
-				<h3 class="box-title">Blocking Page</h3>
-			</div>
-			<div class="box-body">
-				<p>Show page with details if a site is blocked</p>
-				<div class="form-group">
-					<div class="radio"><label><input type="radio" name="blockingPage" value="Yes" <?php if($pishowBlockPage){ ?>checked<?php } ?> disabled>Yes</label></div>
-					<div class="radio"><label><input type="radio" name="blockingPage" value="No" <?php if(!$pishowBlockPage){ ?>checked<?php } ?> disabled>No</label></div>
-					<p>If Yes: Hide content for in page ads?</p>
-					<div class="checkbox"><label><input type="checkbox" <?php if($pishowBlockPageInpage) { ?>checked<?php } ?> disabled>Enabled (recommended)</label></div>
-				</div>
-			</div>
-		</div>
+<?php /* This should be deleted or moved into a separate settings script file...!
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">Blocking Page</h3>
+            </div>
+            <div class="box-body">
+                <p>Show page with details if a site is blocked</p>
+                <div class="form-group">
+                    <div class="radio"><label><input type="radio" name="blockingPage" value="Yes" <?php if($pishowBlockPage){ ?>checked<?php } ?> disabled>Yes</label></div>
+                    <div class="radio"><label><input type="radio" name="blockingPage" value="No" <?php if(!$pishowBlockPage){ ?>checked<?php } ?> disabled>No</label></div>
+                    <p>If Yes: Hide content for in page ads?</p>
+                    <div class="checkbox"><label><input type="checkbox" <?php if($pishowBlockPageInpage) { ?>checked<?php } ?> disabled>Enabled (recommended)</label></div>
+                </div>
+            </div>
+        </div>
 */ ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_sysadmin.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_ftl.php' ?>
-		
-		<?php require 'scripts/pi-hole/php/settings_teleporter.php' ?>
-		
-	</div>
-	
-	<?php
-		}
-	?>
+        
+        <?php require 'scripts/pi-hole/php/settings_sysadmin.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_ftl.php' ?>
+        
+        <?php require 'scripts/pi-hole/php/settings_teleporter.php' ?>
+        
+    </div>
+    
+    <?php
+        }
+    ?>
 
 </div>
 
