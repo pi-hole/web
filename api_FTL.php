915ec26547bc4e4793d2371ca18f5ea36ce982dc
DL6ER <dl6er@dl6er.de>
2017-05-12 18:11:55 +0200

    Define $over_time as array. Fixes #501

--- api_FTL.php
+++ api_FTL.php
@@ -233,6 +233,7 @@ if (isset($_GET['overTimeDataForwards']) && $auth)
 {
 	sendRequestFTL("ForwardedoverTime");
 	$return = getResponseFTL();
+	$over_time = array();
 
 	foreach($return as $line)
 	{
@@ -271,6 +272,7 @@ if (isset($_GET['overTimeDataQueryTypes']) && $auth)
 {
 	sendRequestFTL("QueryTypesoverTime");
 	$return = getResponseFTL();
+	$over_time = array();
 
 	foreach($return as $line)
 	{
