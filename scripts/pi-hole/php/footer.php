ceac8cf2d12fe0712018707307208c153fb975a9
TheME <andreas_fl@outlook.com>
2017-06-04 18:41:04 +0200

    footer.php: edited as discussed

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -42,7 +42,7 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
 	<!-- Version Infos -->
-        <div class="pull-right hidden-xs <?php if(isset($core_commit) || isset($web_commit)) { ?>hidden-sm<?php } ?>">
+        <div class="pull-right hidden-xs hidden-sm<?php if(isset($core_commit) || isset($web_commit)) { ?> hidden-md<?php } ?>">
             <b>Pi-hole Version </b> <?php
             echo $core_current;
             if(isset($core_commit)) { echo " (".$core_branch.", ".$core_commit.")"; }
