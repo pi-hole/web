cfaf12cd638136b8e57576b1eb6485569d714e6a
TheME <andreas_fl@outlook.com>
2017-06-06 18:35:19 +0200

    Update queryads.js

--- scripts/pi-hole/js/queryads.js
+++ scripts/pi-hole/js/queryads.js
@@ -122,3 +122,20 @@ $("#btnSearchExact").on("click", function() {
     exact = "exact";
     eventsource();
 });
+
+// Wrap form-group's buttons to next line when viewed on a small screen
+$(window).on('resize',function() {
+    if ($(window).width() < 991) {
+        $(".form-group.input-group").removeClass("input-group").addClass("input-group-block");
+        $(".form-group.input-group-block > input").css("margin-bottom", "5px");
+        $(".input-group-btn").removeClass("input-group-btn").addClass("input-group-btn-block btn-group");
+    }
+    else {
+        $(".form-group.input-group-block").removeClass("input-group-block").addClass( "input-group" );
+        $(".form-group.input-group > input").css("margin-bottom","");
+        $(".input-group-btn-block").removeClass("input-group-btn-block btn-group").addClass("input-group-btn");
+    }
+});
+$(document).ready(function() {
+    $(window).trigger('resize');
+});
