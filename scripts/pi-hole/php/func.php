6a5e503fe9181581cf24fdd259c3fbdb894588d2
DL6ER <dl6er@dl6er.de>
2017-06-29 13:05:40 +0200

    Allow underscore ( _ ) in domains. Fixes unreported issue with adding domains like `pv6_1-cxl0-c012.1.uvw.xyz.net`
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/php/func.php
+++ scripts/pi-hole/php/func.php
@@ -8,7 +8,7 @@
 
 function is_valid_domain_name($domain_name)
 {
-    return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) &&  //valid chars check
+    return (preg_match("/^([a-z_\d](-*[a-z_\d])*)(\.([a-z_\d](-*[a-z_\d])*))*$/i", $domain_name) &&  //valid chars check
         preg_match("/^.{1,253}$/", $domain_name) && //overall length check
         preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)); //length of each label
 }
