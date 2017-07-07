0de54904f03e6ae0d575f83cdc10efbe832db87a
DL6ER <dl6er@dl6er.de>
2017-07-07 23:02:35 +0200

    Fix comment + another :codacy:

--- api_db.php
+++ api_db.php
@@ -185,18 +185,7 @@ if (isset($_GET['getGraphData']) && $auth)
 			$interval = $q;
 	}
 
-
 	// Count permitted queries in intervals
-	$results = $db->query('SELECT (timestamp/'.$interval.')*'.$interval.' interval, COUNT(*) FROM queries WHERE (status == 1 OR status == 4 OR status == 5)'.$limit.' GROUP by interval ORDER by interval');
-	$addomains = array();
-	while ($row = $results->fetchArray())
-	{
-		$addomains[$row[0]] = intval($row[1]);
-	}
-	$result = array('ads_over_time' => $addomains);
-	$data = array_merge($data, $result);
-
-	// Count blocked queries in intervals
 	$results = $db->query('SELECT (timestamp/'.$interval.')*'.$interval.' interval, COUNT(*) FROM queries WHERE (status == 2 OR status == 3)'.$limit.' GROUP by interval ORDER by interval');
 	$domains = array();
 	while ($row = $results->fetchArray())
@@ -204,6 +193,16 @@ if (isset($_GET['getGraphData']) && $auth)
 		$domains[$row[0]] = intval($row[1]);
 	}
 	$result = array('domains_over_time' => $domains);
+	$data = array_merge($data, $result);
+
+	// Count blocked queries in intervals
+	$results = $db->query('SELECT (timestamp/'.$interval.')*'.$interval.' interval, COUNT(*) FROM queries WHERE (status == 1 OR status == 4 OR status == 5)'.$limit.' GROUP by interval ORDER by interval');
+	$addomains = array();
+	while ($row = $results->fetchArray())
+	{
+		$addomains[$row[0]] = intval($row[1]);
+	}
+	$result = array('ads_over_time' => $addomains);
 	$data = array_merge($data, $result);
 }
 
