e74efe65452e5fd809e8bc0ebde17d121666f1e3
DL6ER <dl6er@dl6er.de>
2017-07-07 17:53:59 +0200

    Add hostname in header

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -235,7 +235,7 @@ if($auth) {
             </a>
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
-                    <!-- User Account: style can be found in dropdown.less -->
+                    <li class="dropdown"><a style="pointer-events:none;"><tt><?php echo gethostname(); ?></tt></a></li>
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
                             <img src="img/logo.svg" class="user-image" style="border-radius: initial" sizes="160x160" alt="Pi-hole logo" />
