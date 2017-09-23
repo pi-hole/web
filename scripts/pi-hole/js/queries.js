2f2b543461e657020b0935c47170a268131d1f9f
DL6ER <dl6er@dl6er.de>
2017-09-23 14:03:55 +0200

    Change cursor style to magnifying glass when hovering single-click searching capable entries to highlight the new feature
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -190,10 +190,13 @@ $(document).ready(function() {
             var api = this.api();
             // Query type IPv4 / IPv6
             api.$('td:eq(1)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
+            api.$("td:eq(1)").css("cursor","zoom-in");
             // Domain
             api.$('td:eq(2)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
+            api.$("td:eq(2)").css("cursor","zoom-in");
             // Client
             api.$('td:eq(3)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
+            api.$("td:eq(3)").css("cursor","zoom-in");
         }
     });
 
