ca04fcaad6c5d9310f83dd374aee7f50b51d00e1
Priit Pihus <priit@pihus.ee>
2017-10-20 23:21:18 +0300

    Make vertical axis start from 0
    Main goal was to change the Y-axis to start from 0 as starting anywhere else could distort the data too much. Good practice is to always start from 0, unless you have a really good reason not to.
    Besides that I removed "var monthNames" which was not used anywhere and "jsonData" was assigned but the referenced variable was never used.
    In addition, I just beautified the code to make it more readable and structured.

--- scripts/pi-hole/js/speedtest.js
+++ scripts/pi-hole/js/speedtest.js
@@ -1,123 +1,121 @@
+var speedlabels = [], downloadspeed = [], uploadspeed = [], speeddata = [], serverPing = [];
 
-var speedlabels = [],downloadspeed=[], uploadspeed=[] ,speeddata = [], serverPing =[];
+function updateSpeedTestData() {
 
-function updateSpeedTestData()
-{
 
-	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
-		  "July", "Aug", "Sept", "Oct", "Nov", "Dec"
-		];
+    function formatDate(itemdate) {
+        return moment(itemdate).format("Do HH:mm");
+    }
 
-	function formatDate(itemdate)
-	{
-		return moment(itemdate).format("Do HH:mm");
-	}
+    $.ajax({
+        url: 'api.php?getSpeedData24hrs&PHP',
+        dataType: 'json'
+    }).done(function (results) {
 
-	var jsonData = $.ajax({
-		url: 'api.php?getSpeedData24hrs&PHP',
-		dataType: 'json',
-	}).done(function (results) {
+        results.forEach(function (packet) {
+            // console.log(speedlabels.indexOf(formatDate(packet.start_time)));
+            if (speedlabels.indexOf(formatDate(packet.start_time)) === -1) {
+                speedlabels.push(formatDate(packet.start_time));
+                uploadspeed.push(parseFloat(packet.upload));
+                downloadspeed.push(parseFloat(packet.download));
+                serverPing.push(parseFloat(packet.server_ping));
 
-			results.forEach(function(packet) {
-				// console.log(speedlabels.indexOf(formatDate(packet.start_time)));
-			if(speedlabels.indexOf(formatDate(packet.start_time)) === -1 )
-				{
-					speedlabels.push(formatDate(packet.start_time));
-					uploadspeed.push(parseFloat(packet.upload));
-					downloadspeed.push(parseFloat(packet.download));
-					serverPing.push(parseFloat(packet.server_ping));
+            }
 
-				}
-
-			});
-			speedChart.update();
-			speeddata = results;
-	});
+        });
+        speedChart.update();
+        speeddata = results;
+    });
 }
 
 
-setInterval(function(){
-	// console.log('updateSpeedTestData');
-  updateSpeedTestData();
+setInterval(function () {
+    // console.log('updateSpeedTestData');
+    updateSpeedTestData();
 }, 6000);
 
 
 var speedChartctx = document.getElementById("speedtestChart");
 var speedChart = new Chart(speedChartctx, {
-	type: 'line',
-	data:
-    {
-  		labels: speedlabels,
-  		datasets: [{
-  			label: 'Download Mbps',
-  			data: downloadspeed,
-  			backgroundColor: 'rgba(75, 192, 192, 0.1)',
-  			borderColor:'rgba(75, 192, 192, 1)',
-  			borderWidth: 1,
-  			cubicInterpolationMode: 'monotone',
-        yAxisID: "y-axis-1",
-  		},
-  		{
-  			label: 'Upload Mbps',
-  			data: uploadspeed,
-  			backgroundColor: 'rgba(255, 99, 132, 0.1)',
-  			borderColor: 'rgba(255,99,132,1)',
-  			borderWidth: 1,
-        yAxisID: "y-axis-1",
-  		},
-      {
-  			label: 'Ping ms',
-  			data: serverPing,
-  			backgroundColor: 'rgba(69,237,33,0.0)',
-  			borderColor: 'rgba(69,237,33,1)',
-  			borderWidth: 1,
-        borderDash: [5, 5],
-        yAxisID: "y-axis-2",
-  		}
+    type: 'line',
+    data: {
+        labels: speedlabels,
+        datasets: [{
+            label: 'Download Mbps',
+            data: downloadspeed,
+            backgroundColor: 'rgba(75, 192, 192, 0.1)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            cubicInterpolationMode: 'monotone',
+            yAxisID: "y-axis-1"
+        },
+            {
+                label: 'Upload Mbps',
+                data: uploadspeed,
+                backgroundColor: 'rgba(255, 99, 132, 0.1)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                yAxisID: "y-axis-1"
+            },
+            {
+                label: 'Ping ms',
+                data: serverPing,
+                backgroundColor: 'rgba(69,237,33,0.0)',
+                borderColor: 'rgba(69,237,33,1)',
+                borderWidth: 1,
+                borderDash: [5, 5],
+                yAxisID: "y-axis-2"
+            }
 
-		]
-	},
-	options: {
-		 responsive: true,
-       		maintainAspectRatio: false,
-		legend : {
-				display : false
-			},
-		scales: {
-			yAxes: [{
-                  type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                  display: true,
-                  position: "left",
-                  id: "y-axis-1",
-              },
-              {
-                  type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                  display: true,
-                  position: "right",
-                  id: "y-axis-2",
-              }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: "left",
+                id: "y-axis-1",
+                ticks : {
+                    min: 0
+                }
+            },
+                {
+                    type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: "right",
+                    id: "y-axis-2",
+                    ticks : {
+                        min: 0
+                    }
+                }
             ],
-      xAxes: [
-              {
-									// type :'time',
-                  display: true,
-                  scaleLabel: {
-                      display: true
-                  },
-									ticks: {
-							        // autoSkip: true,
-							        maxTicksLimit: 10,
-											maxRotation: 0,
-        							minRotation: 0
-							    }
-              }
+            xAxes: [
+                {
+                    // type :'time',
+                    display: true,
+                    scaleLabel: {
+                        display: true
+                    },
+                    ticks: {
+                        // autoSkip: true,
+                        maxTicksLimit: 10,
+                        maxRotation: 0,
+                        minRotation: 0
+                    }
+                }
             ]
-		       },
-    tooltips: {
-      enabled : true,
-        mode: "x-axis",
-        intersect: false,
+        },
+        tooltips: {
+            enabled: true,
+            mode: "x-axis",
+            intersect: false
+        }
     }
-	}
 });
 updateSpeedTestData();
