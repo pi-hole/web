2ff11f4301bafee94dce8b3ebe5f9b3647596e6f
DL6ER <dl6er@dl6er.de>
2017-10-21 12:35:04 +0200

    Add box for API settings
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- settings.php
+++ settings.php
@@ -250,7 +250,6 @@ if (isset($setupVars["API_PRIVACY_MODE"])) {
                             <form role="form" method="post">
                                 <div class="row">
                                     <div class="col-md-12">
-
                                         <table class="table table-striped">
                                             <thead>
                                             <tr>
@@ -841,57 +840,63 @@ if (isset($setupVars["API_PRIVACY_MODE"])) {
                     <form role="form" method="post">
                         <div class="row">
                             <div class="col-lg-12">
-                                <h4>Top Lists</h4>
-                                <p>Exclude the following domains from being shown in</p>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label>Top Domains / Top Advertisers</label>
-                                    <textarea name="domains" class="form-control" rows="4"
-                                              placeholder="Enter one domain per line"><?php foreach ($excludedDomains as $domain) {
-                                            echo $domain . "\n";
-                                        } ?></textarea>
+                                <div class="box box-warning">
+                                    <div class="box-header with-border">
+                                        <h3 class="box-title">API settings</h3>
+                                    </div>
+                                    <div class="box-body">
+                                        <h4>Top Lists</h4>
+                                        <p>Exclude the following domains from being shown in</p>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label>Top Domains / Top Advertisers</label>
+                                                <textarea name="domains" class="form-control" rows="4"
+                                                          placeholder="Enter one domain per line"><?php foreach ($excludedDomains as $domain) {
+                                                        echo $domain . "\n";
+                                                    } ?></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label>Top Clients</label>
+                                                <textarea name="clients" class="form-control" rows="4"
+                                                          placeholder="Enter one IP address or host name per line"><?php foreach ($excludedClients as $client) {
+                                                        echo $client . "\n";
+                                                    } ?></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <h4>Privacy settings (Statistics / Query Log)</h4>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <div class="checkbox"><label><input type="checkbox"
+                                                                                    name="querylog-permitted"
+                                                                                    <?php if ($queryLog === "permittedonly" || $queryLog === "all"){ ?>checked<?php } ?>>
+                                                        Show permitted domain entries</label></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <div class="checkbox"><label><input type="checkbox" name="querylog-blocked"
+                                                                                    <?php if ($queryLog === "blockedonly" || $queryLog === "all"){ ?>checked<?php } ?>>
+                                                        Show blocked domain entries</label></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <h4>Privacy mode</h4>
+                                            <div class="form-group">
+                                                <div class="checkbox"><label><input type="checkbox" name="privacyMode"
+                                                                                    <?php if ($privacyMode){ ?>checked<?php } ?>>
+                                                        Don't
+                                                        show origin of DNS requests in query log</label></div>
+                                        </div>
+                                        <input type="hidden" name="field" value="API">
+                                        <input type="hidden" name="token" value="<?php echo $token ?>">
+                                        <button type="button" class="btn btn-primary api-token">Show API token</button>
+                                        <button type="submit" class="btn btn-primary pull-right">Save</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label>Top Clients</label>
-                                    <textarea name="clients" class="form-control" rows="4"
-                                              placeholder="Enter one IP address or host name per line"><?php foreach ($excludedClients as $client) {
-                                            echo $client . "\n";
-                                        } ?></textarea>
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <h4>Privacy settings (Statistics / Query Log)</h4>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <div class="checkbox"><label><input type="checkbox"
-                                                                        name="querylog-permitted"
-                                                                        <?php if ($queryLog === "permittedonly" || $queryLog === "all"){ ?>checked<?php } ?>>
-                                            Show permitted domain entries</label></div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <div class="checkbox"><label><input type="checkbox" name="querylog-blocked"
-                                                                        <?php if ($queryLog === "blockedonly" || $queryLog === "all"){ ?>checked<?php } ?>>
-                                            Show blocked domain entries</label></div>
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <h4>Privacy mode</h4>
-                                <div class="form-group">
-                                    <div class="checkbox"><label><input type="checkbox" name="privacyMode"
-                                                                        <?php if ($privacyMode){ ?>checked<?php } ?>>
-                                            Don't
-                                            show origin of DNS requests in query log</label></div>
-                                </div>
-                                <input type="hidden" name="field" value="API">
-                                <input type="hidden" name="token" value="<?php echo $token ?>">
-                                <button type="button" class="btn btn-primary api-token">Show API token</button>
-                                <button type="submit" class="btn btn-primary pull-right">Save</button>
                             </div>
                         </div>
                     </form>
