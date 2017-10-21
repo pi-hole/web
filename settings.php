0c70ad5895030a8649d3de4b4ead18f725ecf088
DL6ER <dl6er@dl6er.de>
2017-10-21 12:40:08 +0200

    Add Teleporter box
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- settings.php
+++ settings.php
@@ -953,52 +953,56 @@ if (isset($setupVars["API_PRIVACY_MODE"])) {
                 <div id="teleporter" class="tab-pane fade">
                     <div class="row">
                         <div class="col-lg-12">
-                            <?php if (extension_loaded('Phar')) { ?>
-                                <form role="form" method="post" id="takeoutform"
-                                      action="scripts/pi-hole/php/teleporter.php"
-                                      target="_blank" enctype="multipart/form-data">
-                                    <input type="hidden" name="token" value="<?php echo $token ?>">
-                                    <div class="col-lg-12">
-                                        <p>Export your Pi-hole lists as downloadable archive</p>
-                                        <button type="submit" class="btn btn-default">Export</button>
-                                        <hr>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <label>Import ...</label>
-                                        <div class="form-group">
-                                            <div class="checkbox">
-                                                <label><input type="checkbox" name="whitelist" value="true"
-                                                              checked>
-                                                    Whitelist</label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label><input type="checkbox" name="blacklist" value="true"
-                                                              checked>
-                                                    Blacklist (exact)</label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label><input type="checkbox" name="wildlist" value="true"
-                                                              checked>
-                                                    Blacklist (wildcard)</label>
+                            <div class="box box-warning">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">Teleporter</h3>
+                                </div>
+                                <div class="box-body">
+                                <?php if (extension_loaded('Phar')) { ?>
+                                    <form role="form" method="post" id="takeoutform"
+                                          action="scripts/pi-hole/php/teleporter.php"
+                                          target="_blank" enctype="multipart/form-data">
+                                        <input type="hidden" name="token" value="<?php echo $token ?>">
+                                        <div class="col-lg-12">
+                                            <p>Export your Pi-hole lists as downloadable archive</p>
+                                            <button type="submit" class="btn btn-default">Export</button>
+                                            <hr>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <label>Import ...</label>
+                                            <div class="form-group">
+                                                <div class="checkbox">
+                                                    <label><input type="checkbox" name="whitelist" value="true"
+                                                                  checked>
+                                                        Whitelist</label>
+                                                </div>
+                                                <div class="checkbox">
+                                                    <label><input type="checkbox" name="blacklist" value="true"
+                                                                  checked>
+                                                        Blacklist (exact)</label>
+                                                </div>
+                                                <div class="checkbox">
+                                                    <label><input type="checkbox" name="wildlist" value="true"
+                                                                  checked>
+                                                        Blacklist (wildcard)</label>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label for="zip_file">File input</label>
-                                            <input type="file" name="zip_file" id="zip_file">
-                                            <p class="help-block">Upload only Pi-hole backup files.</p>
-                                            <button type="submit" class="btn btn-default" name="action"
-                                                    value="in">Import
-                                            </button>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label for="zip_file">File input</label>
+                                                <input type="file" name="zip_file" id="zip_file">
+                                                <p class="help-block">Upload only Pi-hole backup files.</p>
+                                                <button type="submit" class="btn btn-default" name="action"
+                                                        value="in">Import
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
-                                </form>
-                            <?php } else { ?>
-                            <p>The PHP extension <code>Phar</code> is not loaded. Please ensure it is installed
-                                and loaded
-                                if you want to use the Pi-hole teleporter.
-                                <?php } ?>
+                                    </form>
+                                <?php } else { ?>
+                                    <p>The PHP extension <code>Phar</code> is not loaded. Please ensure it is installed and loaded if you want to use the Pi-hole teleporter.</p><?php } ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
