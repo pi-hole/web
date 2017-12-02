aa42f3d282835572e78fd1e157c763c2f8d4a598
DL6ER <dl6er@dl6er.de>
2017-12-02 11:16:20 +0100

    :codacy:
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -189,24 +189,24 @@ $(document).ready(function() {
         "initComplete": function () {
             var api = this.api();
             // Query type IPv4 / IPv6
-            api.$('td:eq(1)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
-            api.$('td:eq(1)').hover(
-              function () { this.title="Click to show only "+this.innerHTML+" queries"; this.style.color="#72afd2" },
-              function () { this.style.color="" }
+            api.$("td:eq(1)").click( function () { api.search( this.innerHTML ).draw(); $("#resetButton").show(); } );
+            api.$("td:eq(1)").hover(
+              function () { this.title="Click to show only "+this.innerHTML+" queries"; this.style.color="#72afd2"; },
+              function () { this.style.color=""; }
             );
             api.$("td:eq(1)").css("cursor","pointer");
             // Domain
-            api.$('td:eq(2)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
-            api.$('td:eq(2)').hover(
-              function () { this.title="Click to show only queries with domain "+this.innerHTML; this.style.color="#72afd2" },
-              function () { this.style.color="" }
+            api.$("td:eq(2)").click( function () { api.search( this.innerHTML ).draw(); $("#resetButton").show(); } );
+            api.$("td:eq(2)").hover(
+              function () { this.title="Click to show only queries with domain "+this.innerHTML; this.style.color="#72afd2"; },
+              function () { this.style.color=""; }
             );
             api.$("td:eq(2)").css("cursor","pointer");
             // Client
-            api.$('td:eq(3)').click( function () { api.search( this.innerHTML ).draw(); $('#resetButton').show(); } );
-            api.$('td:eq(3)').hover(
-              function () { this.title="Click to show only queries made by "+this.innerHTML; this.style.color="#72afd2" },
-              function () { this.style.color="" }
+            api.$("td:eq(3)").click( function () { api.search( this.innerHTML ).draw(); $("#resetButton").show(); } );
+            api.$("td:eq(3)").hover(
+              function () { this.title="Click to show only queries made by "+this.innerHTML; this.style.color="#72afd2"; },
+              function () { this.style.color=""; }
             );
             api.$("td:eq(3)").css("cursor","pointer");
         }
@@ -224,7 +224,7 @@ $(document).ready(function() {
         }
     } );
 
-    $('#resetButton').click( function () { tableApi.search("").draw(); $('#resetButton').hide(); } );
+    $("#resetButton").click( function () { tableApi.search("").draw(); $("#resetButton").hide(); } );
 } );
 
 
