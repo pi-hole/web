848f59787a5d5f351b15844c892a88e03158823e
DL6ER <dl6er@dl6er.de>
2017-12-10 12:31:54 +0100

    Add checkbox on Query Log to be able to disable filtering on click
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- queries.php
+++ queries.php
@@ -134,6 +134,7 @@ if(strlen($showing) > 0)
                     </tfoot>
                 </table>
             </div>
+          <label><input type="checkbox" id="autofilter" checked="true">&nbsp;Apply filtering on click on Type, Domain, and Clients</label><br/>
           <button type="button" id="resetButton" hidden="true">Clear Filters</button>
        </div>
         <!-- /.box-body -->
