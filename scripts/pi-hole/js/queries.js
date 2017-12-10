7a93fa647050b0b024bee9672d5611f06c1500c1
DL6ER <dl6er@dl6er.de>
2017-12-10 12:43:59 +0100

    :codacy: says: "Strings must use doublequote"
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -193,22 +193,22 @@ $(document).ready(function() {
         "initComplete": function () {
             var api = this.api();
             // Query type IPv4 / IPv6
-            api.$('td:eq(1)').click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $('#resetButton').show(); }});
-            api.$('td:eq(1)').hover(
+            api.$("td:eq(1)").click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $("#resetButton").show(); }});
+            api.$("td:eq(1)").hover(
               function () { this.title="Click to show only "+this.innerHTML+" queries"; this.style.color="#72afd2" },
               function () { this.style.color="" }
             );
             api.$("td:eq(1)").css("cursor","pointer");
             // Domain
-            api.$('td:eq(2)').click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $('#resetButton').show(); }});
-            api.$('td:eq(2)').hover(
+            api.$("td:eq(2)").click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $("#resetButton").show(); }});
+            api.$("td:eq(2)").hover(
               function () { this.title="Click to show only queries with domain "+this.innerHTML; this.style.color="#72afd2" },
               function () { this.style.color="" }
             );
             api.$("td:eq(2)").css("cursor","pointer");
             // Client
-            api.$('td:eq(3)').click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $('#resetButton').show(); }});
-            api.$('td:eq(3)').hover(
+            api.$("td:eq(3)").click( function () { if(autofilter()){ api.search( this.innerHTML ).draw(); $("#resetButton").show(); }});
+            api.$("td:eq(3)").hover(
               function () { this.title="Click to show only queries made by "+this.innerHTML; this.style.color="#72afd2" },
               function () { this.style.color="" }
             );
@@ -228,7 +228,7 @@ $(document).ready(function() {
         }
     } );
 
-    $('#resetButton').click( function () { tableApi.search("").draw(); $('#resetButton').hide(); } );
+    $("#resetButton").click( function () { tableApi.search("").draw(); $("#resetButton").hide(); } );
 } );
 
 
