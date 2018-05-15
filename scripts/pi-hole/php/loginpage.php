967763b6dacedfae6d62f3078b957c35ec61c7a8
Rob Gill <rrobgill@protonmail.com>
2018-05-15 10:56:49 +1000

    indenting
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- scripts/pi-hole/php/loginpage.php
+++ scripts/pi-hole/php/loginpage.php
@@ -37,7 +37,7 @@
           <div class="col-xs-4">
             <div class="form-group">
               <div class="checkbox pull-right"><label><input type="checkbox" id="logincookie" name="persistentlogin">Remember me for 7 days</label></div>
-              </div>
+            </div>
             <button type="submit" href="#" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-log-in"></i>&nbsp;&nbsp;&nbsp;Log in</button>
           </div>
         </div>
