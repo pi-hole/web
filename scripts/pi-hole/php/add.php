99b0535f8e3c8517acc8ef0ff6b97731fd5a12e9
DL6ER <dl6er@dl6er.de>
2018-11-29 18:41:30 +0100

    Remove empty line at beginning of output of add.php
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/php/add.php
+++ scripts/pi-hole/php/add.php
@@ -4,9 +4,7 @@
 *  Network-wide ad blocking via your own hardware.
 *
 *  This file is copyright under the latest version of the EUPL.
-*  Please see LICENSE file for your rights under this license. */ ?>
-
-<?php
+*  Please see LICENSE file for your rights under this license. */
 require_once('auth.php');
 
 $type = $_POST['list'];
