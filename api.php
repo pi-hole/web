cc598af8f3b6003f79b9fabfeac819675f15b799
DL6ER <dl6er@dl6er.de>
2019-02-03 18:45:44 +0100

    2017 -> 2019
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 /*   Pi-hole: A black hole for Internet advertisements
-*    (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*    (c) 2019 Pi-hole, LLC (https://pi-hole.net)
 *    Network-wide ad blocking via your own hardware.
 *
 *    This file is copyright under the latest version of the EUPL.
