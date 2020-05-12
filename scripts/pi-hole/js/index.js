d10f1905cc7006746e68e80ee68b6debdebed413
Adam Warner <me@adamwarner.co.uk>
2020-05-12 13:21:30 +0100

    fix prettier complaints
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- scripts/pi-hole/js/index.js
+++ scripts/pi-hole/js/index.js
@@ -10,22 +10,24 @@
 var timeLineChart, clientsChart;
 var queryTypePieChart, forwardDestinationPieChart;
 
-var colors = [ "#3c8dbc",
-    "#f56954",
-    "#00a65a",
-    "#00c0ef",
-    "#f39c12",
-    "#0073b7",
-    "#001F3F",
-    "#39CCCC",
-    "#3D9970",
-    "#01FF70",
-    "#FF851B",
-    "#F012BE",
-    "#8E24AA",
-    "#D81B60",
-    "#222222",
-    "#d2d6de"];
+var colors = [
+  "#3c8dbc",
+  "#f56954",
+  "#00a65a",
+  "#00c0ef",
+  "#f39c12",
+  "#0073b7",
+  "#001F3F",
+  "#39CCCC",
+  "#3D9970",
+  "#01FF70",
+  "#FF851B",
+  "#F012BE",
+  "#8E24AA",
+  "#D81B60",
+  "#222222",
+  "#d2d6de"
+];
 
 function padNumber(num) {
   return ("00" + num).substr(-2, 2);
