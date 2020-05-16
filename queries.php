821b283cf248a78d0593b0fc99e345d8819fa9cb
XhmikosR <xhmikosr@gmail.com>
2020-05-16 18:53:32 +0300

    Fix queries Clear Filters button
    Now it should look like the rest of the buttons
    
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- queries.php
+++ queries.php
@@ -147,7 +147,7 @@ if(strlen($showing) > 0)
                 </tfoot>
             </table>
             <label><input type="checkbox" id="autofilter">&nbsp;Apply filtering on click on Type, Domain, and Clients</label><br/>
-            <button type="button" id="resetButton" hidden="true">Clear Filters</button>
+            <button type="button" id="resetButton" hidden="true" class="btn btn-default btn-sm text-red">Clear Filters</button>
         </div>
         <!-- /.box-body -->
       </div>
