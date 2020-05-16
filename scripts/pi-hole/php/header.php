d062cb0423860a160e33ee55bef43dd0cad391d8
XhmikosR <xhmikosr@gmail.com>
2020-05-16 19:53:44 +0300

    header: fix `aria-expanded` for user-menu dropdown
    It's not expanded by default, so it needs `aria-expanded="false"`
    
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -277,7 +277,7 @@ if($auth) {
                         </a>
                     </li>
                     <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                             <img src="img/logo.svg" class="user-image" alt="Pi-hole logo" style="border-radius: 0" width="25" height="25">
                             <span class="hidden-xs">Pi-hole</span>
                         </a>
