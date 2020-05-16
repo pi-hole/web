b416075cdcd6c9bf8adfa9de3f682b4ba6e52016
XhmikosR <xhmikosr@gmail.com>
2020-05-16 19:16:44 +0300

    header: Move license header at the top
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -166,13 +166,13 @@
 
     $piholeFTLConf = piholeFTLConfig();
 ?>
-<!doctype html>
 <!-- Pi-hole: A black hole for Internet advertisements
 *  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
 *  Network-wide ad blocking via your own hardware.
 *
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. -->
+<!doctype html>
 <html lang="en">
 <head>
 <?php if ($darkmode) { ?>
