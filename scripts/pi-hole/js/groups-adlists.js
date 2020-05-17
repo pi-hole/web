f2b11caf587f48b375cf9099af515f8d88655a47
XhmikosR <xhmikosr@gmail.com>
2020-05-17 08:53:42 +0300

    Rename variable to camelcase.
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- scripts/pi-hole/js/groups-adlists.js
+++ scripts/pi-hole/js/groups-adlists.js
@@ -127,8 +127,8 @@ function initTable() {
         })
         .on("changed.bs.select", function () {
           // enable Apply button
-          if ($(ApplyBtn).prop("disabled")) {
-            $(ApplyBtn)
+          if ($(applyBtn).prop("disabled")) {
+            $(applyBtn)
               .addClass("btn-success")
               .prop("disabled", false)
               .on("click", function () {
@@ -138,9 +138,9 @@ function initTable() {
         })
         .on("hide.bs.select", function () {
           // Restore values if drop-down menu is closed without clicking the Apply button
-          if (!$(ApplyBtn).prop("disabled")) {
+          if (!$(applyBtn).prop("disabled")) {
             $(this).val(data.groups).selectpicker("refresh");
-            $(ApplyBtn).removeClass("btn-success").prop("disabled", true).off("click");
+            $(applyBtn).removeClass("btn-success").prop("disabled", true).off("click");
           }
         })
         .selectpicker()
@@ -152,7 +152,7 @@ function initTable() {
             ' class="btn btn-block btn-sm" disabled>Apply</button>'
         );
 
-      var ApplyBtn = "#btn_apply_" + data.id;
+      var applyBtn = "#btn_apply_" + data.id;
 
       var button =
         '<button type="button" class="btn btn-danger btn-xs" id="deleteAdlist_' +
