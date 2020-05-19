67da8474406b10ed3b9efb6406ad5c77bdb0b149
DL6ER <dl6er@dl6er.de>
2020-05-19 11:27:10 +0200

    Rename IP column on groups->clients page.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- groups-clients.php
+++ groups-clients.php
@@ -63,7 +63,7 @@
                     <thead>
                     <tr>
                         <th>ID</th>
-                        <th>IP address</th>
+                        <th title="Acceptable values are: IP address, subnet (CIDR notation), MAC address (AA:BB:CC:DD:EE:FF format) or host names.">Client</th>
                         <th>Comment</th>
                         <th>Group assignment</th>
                         <th>Action</th>
