30776108ee0979457a0f8bbf5d96f35823667371
XhmikosR <xhmikosr@gmail.com>
2020-05-24 21:12:15 +0300

    Use `fa5` class on the sidebar toggle
    This was added in AdminLTE v2.4.13.
    
    Also, remove unneeded related custom CSS while at it.
    
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -265,7 +265,7 @@ if($auth) {
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
             <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <a href="#" class="sidebar-toggle fa5" data-toggle="push-menu" role="button">
                 <span class="sr-only">Toggle navigation</span>
             </a>
             <div class="navbar-custom-menu">
