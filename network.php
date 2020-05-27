2464ffe71f197adb006072a1483d36b3983349de
XhmikosR <xhmikosr@gmail.com>
2020-05-27 18:18:30 +0300

    network.php: move the scripts above footer
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- network.php
+++ network.php
@@ -73,10 +73,10 @@ $token = $_SESSION['token'];
 </div>
 <!-- /.row -->
 
-<?php
-    require "scripts/pi-hole/php/footer.php";
-?>
-
 <script src="scripts/pi-hole/js/utils.js"></script>
 <script src="scripts/pi-hole/js/ip-address-sorting.js"></script>
 <script src="scripts/pi-hole/js/network.js"></script>
+
+<?php
+    require "scripts/pi-hole/php/footer.php";
+?>
