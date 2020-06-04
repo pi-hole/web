4a101fa3a869bd080ad18f21546287cfd4e3918b
XhmikosR <xhmikosr@gmail.com>
2020-06-04 16:09:34 +0300

    Tailog pages: make checkboxes use the same markup as everywhere else.
    This is still not proper, but at least it's consistent.
    
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- taillog-FTL.php
+++ taillog-FTL.php
@@ -12,13 +12,17 @@
     <h1>Output the last lines of the pihole-FTL.log file (live)</h1>
 </div>
 
-<input type="checkbox" checked id="chk1">
-<label for="chk1">Automatic scrolling on update</label>
+<div>
+    <input type="checkbox" checked id="chk1">
+    <label for="chk1">Automatic scrolling on update</label>
+</div>
 
 <pre id="output" style="width: 100%; height: 100%; max-height:650px; overflow-y:scroll;"></pre>
 
-<input type="checkbox" checked id="chk2">
-<label for="chk2">Automatic scrolling on update</label>
+<div>
+    <input type="checkbox" checked id="chk2">
+    <label for="chk2">Automatic scrolling on update</label>
+</div>
 
 <script src="scripts/pi-hole/js/taillog-FTL.js"></script>
 
