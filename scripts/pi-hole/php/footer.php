52d3c33c0f1c7a005032814daee7a58ac5385ed2
yubiuser <ckoenig@posteo.de>
2020-08-14 20:47:22 +0200

    Revert "Add 'seconds' to custom disable timeout modal"
    This reverts commit c15a8ecde2cf487c261ecc8b90ca90a1fe053203.
    
    Signed-off-by: yubiuser <ckoenig@posteo.de>

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -15,7 +15,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Custom disable timeout (seconds)</h4>
+                    <h4 class="modal-title" id="myModalLabel">Custom disable timeout</h4>
                 </div>
                 <div class="modal-body">
                     <div class="input-group">
