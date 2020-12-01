8728d5b738fc8e10293b7fc4331db103991a560c
dnhp <34394848+dnhp@users.noreply.github.com>
2020-12-01 09:56:15 +0000

    Fix issue 1643 - Quote cases 12 and 13 to stop switch falling through to default case when we have retried DNS queries
    Signed-off-by: dnhp <34394848+dnhp@users.noreply.github.com>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -185,12 +185,12 @@ $(function () {
             '<button type="button" class="btn btn-default btn-sm text-green"><i class="fas fa-check"></i> Whitelist</button>';
           isCNAME = true;
           break;
-        case 12:
+        case "12":
           colorClass = "text-green";
           fieldtext = "Retried";
           buttontext = "";
           break;
-        case 13:
+        case "13":
           colorClass = "text-green";
           fieldtext = "Retried <br class='hidden-lg'>(ignored)";
           buttontext = "";
