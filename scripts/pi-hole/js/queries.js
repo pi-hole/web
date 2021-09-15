48c63c0a0be85a2ae94732498e5149d32e8f2836
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-09-15 20:34:05 +0000

    Fix spelling
    Signed-off-by: a1346054 <36859588+a1346054@users.noreply.github.com>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -32,9 +32,9 @@ function handleAjaxError(xhr, textStatus) {
   if (textStatus === "timeout") {
     alert("The server took too long to send the data.");
   } else if (xhr.responseText.indexOf("Connection refused") !== -1) {
-    alert("An error occured while loading the data: Connection refused. Is FTL running?");
+    alert("An error occurred while loading the data: Connection refused. Is FTL running?");
   } else {
-    alert("An unknown error occured while loading the data.\n" + xhr.responseText);
+    alert("An unknown error occurred while loading the data.\n" + xhr.responseText);
   }
 
   $("#all-queries_processing").hide();
