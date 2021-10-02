28b7e853737825b6462ac53f5677d146f86445fa
Lion Ralfs <lion.ralfs@gmail.com>
2021-10-02 15:31:41 +0200

    fix: exclude status checkboxes from being treated as iCheck elements
    Signed-off-by: Lion Ralfs <lion.ralfs@gmail.com>

--- scripts/pi-hole/js/footer.js
+++ scripts/pi-hole/js/footer.js
@@ -137,8 +137,9 @@ function initCheckboxRadioStyle() {
 
   function applyCheckboxRadioStyle(style) {
     boxsheet.attr("href", getCheckboxURL(style));
-    // Get all radio/checkboxes for theming, with the exception of the two radio buttons on the custom disable timer
-    var sel = $("input[type='radio'],input[type='checkbox']").not("#selSec").not("#selMin");
+    // Get all radio/checkboxes for theming, with the exception of the two radio buttons on the custom disable timer,
+    // as well as every element with an id that starts with "status_"
+    var sel = $("input[type='radio'],input[type='checkbox']").not("#selSec").not("#selMin").not('[id^=status_]');
     sel.parent().removeClass();
     sel.parent().addClass("icheck-" + style);
   }
