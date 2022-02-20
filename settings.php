304a4e31a85e8bb102eee6017f3b78e3511d1bc7
yubiuser <ckoenig@posteo.de>
2022-02-20 18:36:27 +0100

    Set min and step for rate_limit_interval as well

--- settings.php
+++ settings.php
@@ -949,7 +949,7 @@ if (isset($_GET['tab']) && in_array($_GET['tab'], array("sysadmin", "dns", "piho
                                                 <br>
                                                 <h4><a id="ratelimit"></a>Rate-limiting</h4>
                                                 <p>Block clients making more than <input type="number" name="rate_limit_count" value="<?=$rate_limit_count?>" min="0" step="10"> queries within
-                                                <input type="number" name="rate_limit_interval" value="<?=$rate_limit_interval?>"> seconds.</p>
+                                                <input type="number" name="rate_limit_interval" value="<?=$rate_limit_interval?>" min="0" step="10"> seconds.</p>
                                                 <p>When a client makes too many queries in too short time, it
                                                 gets rate-limited. Rate-limited queries are answered with a
                                                 <code>REFUSED</code> reply and not further processed by FTL
