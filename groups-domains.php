b33019560844a64d7a65bdc4249f965e3e8d695b
Martin Brose <13284268+martinbrose@users.noreply.github.com>
2022-04-24 23:02:02 +0100

    Added note to regular expresions also.
    Signed-off-by: Martin Brose <13284268+martinbrose@users.noreply.github.com>

--- groups-domains.php
+++ groups-domains.php
@@ -90,6 +90,11 @@
                                         <input id="new_regex_comment" type="text" class="form-control" placeholder="Description (optional)">
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>Note: The regular expression will be assigned to the Default Group. Other groups will need to be selected within Group Management > Domains.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
