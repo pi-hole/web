b6963a8eb19bf27702fbf3f6d5d2750a676a665d
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-08-24 09:26:46 -0700

    Add auto focus to groups and groups-adlist pages
    Signed-off-by: Joshua Wu <Joshua.wu.0910@gmail.com>

--- scripts/pi-hole/js/groups-adlists.js
+++ scripts/pi-hole/js/groups-adlists.js
@@ -517,6 +517,7 @@ function addAdlist() {
         $("#new_comment").val("");
         table.ajax.reload();
         table.rows().deselect();
+        $("#new_address").focus();
       } else {
         utils.showAlert("error", "", "Error while adding new adlist: ", response.message);
       }
