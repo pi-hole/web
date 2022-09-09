cce2a27b4ee826660d3ba7b3ecdac182971755e5
Peter Russell <jpgpi250@gmail.com>
2022-09-09 16:07:49 +0200

    Update scripts/pi-hole/js/queries.js
    Co-authored-by: yubiuser <ckoenig@posteo.de>
    Signed-off-by: Peter Russell <jpgpi250@gmail.com>

--- scripts/pi-hole/js/queries.js
+++ scripts/pi-hole/js/queries.js
@@ -116,7 +116,7 @@ $(function () {
         regexLink = false;
 
       // accompanies Store domainlist IDs for blocked/permitted queries FTL PR 1409
-      if (data.length > 9 && !isNaN(data[9]) && data[9] > 0) {
+      if (data.length > 9 && Number.isInteger(data[9]) && data[9] > 0) {
         regexLink = true;
       }
 
