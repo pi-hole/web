1a0b24474d032827176978afd9310ec3a60316c2
DL6ER <dl6er@dl6er.de>
2022-11-07 14:04:14 +0100

    Add stale cache query status
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/db_queries.js
+++ scripts/pi-hole/js/db_queries.js
@@ -167,6 +167,10 @@ function excludeStatusTypes() {
     statusType.push(16);
   }
 
+  if ($("#type_cached_stale").prop("checked") === false) {
+    statusType.push(17);
+  }
+
   return statusType.join(",");
 }
 
@@ -348,6 +352,13 @@ $(function () {
           fieldtext =
             "<span class='text-orange'>Blocked <br class='hidden-lg'>(special domain)</span>";
           blocked = true;
+          break;
+        case 17:
+          fieldtext =
+            "<span class='text-orange'>OK</span> <br class='hidden-lg'>(stale cache)" +
+            dnssecStatus;
+          buttontext =
+            '<button type="button" class="btn btn-default btn-sm text-red"><i class="fa fa-ban"></i> Blacklist</button>';
           break;
         default:
           fieldtext = "Unknown (" + parseInt(data[4], 10) + ")";
