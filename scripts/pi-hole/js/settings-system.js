a69726b4dac5b8f4d529a697ecb0e6d039c9a7b4
DL6ER <dl6er@dl6er.de>
2023-06-14 15:17:56 +0200

    Fix typo and clarify the expired cache entries count
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- scripts/pi-hole/js/settings-system.js
+++ scripts/pi-hole/js/settings-system.js
@@ -162,7 +162,7 @@ function updateMetrics() {
       // Set metrics
       setMetrics(metrics, "sysinfo-");
 
-      $("#cache-utilization").text(((100 * cacheEntries) / cacheSize).toFixed(1) + "%");
+      $("#cache-utilization").text(cacheEntries.toLocaleString() + " (" + ((100 * cacheEntries) / cacheSize).toFixed(1) + "%)");
 
       $("div[id^='sysinfo-metrics-overlay']").hide();
       // Update every 10 seconds
