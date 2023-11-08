305a7d36f29bdd49709f171f65053d5c77db3347
Adam Warner <me@adamwarner.co.uk>
2023-11-08 22:12:25 +0000

    Set the title icons correctly on the local dns/cname page

--- scripts/pi-hole/js/settings.js
+++ scripts/pi-hole/js/settings.js
@@ -5,7 +5,7 @@
  *  This file is copyright under the latest version of the EUPL.
  *  Please see LICENSE file for your rights under this license. */
 
-/* global utils:false, apiFailure:false */
+/* global utils:false, apiFailure:false*/
 
 $(function () {
   // Handle hiding of alerts
