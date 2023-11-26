c8a43d3daa30518aedf283c67fe448e691fcf1c7
Christian König <ckoenig@posteo.de>
2023-11-26 20:48:15 +0100

    Revert "Update datatables (#2852)"
    This reverts commit e4ac12c3e08313f55af7af000e36acd4475e52a6, reversing
    changes made to 54dba571611b7db192afd0add00f871f89a3e865.
    
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- scripts/pi-hole/js/network.js
+++ scripts/pi-hole/js/network.js
@@ -203,7 +203,7 @@ $(function () {
     order: [[6, "desc"]],
     columns: [
       { data: "id", visible: false },
-      { data: "ips[].ip", type: "ip-address", width: "25%" },
+      { data: "ip", type: "ip-address", width: "25%" },
       { data: "hwaddr", width: "10%" },
       { data: "interface", width: "4%" },
       {
