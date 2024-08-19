afdb1a77291efb8edf7aaa82fb460e23e383c508
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2024-08-19 19:55:01 +0200

    Implement Trusted Types by passing in-use injection sinks through DOMPurify
    Signed-off-by: Orazio <22700499+orazioedoardo@users.noreply.github.com>

---
+++ scripts/pi-hole/js/trusted-types-policies.js
@@ -0,0 +1,22 @@
+/* Pi-hole: A black hole for Internet advertisements
+ *  (c) 2023 Pi-hole, LLC (https://pi-hole.net)
+ *  Network-wide ad blocking via your own hardware.
+ *
+ *  This file is copyright under the latest version of the EUPL.
+ *  Please see LICENSE file for your rights under this license. */
+
+// See https://github.com/cure53/DOMPurify?tab=readme-ov-file#what-about-dompurify-and-trusted-types
+// on why, counterintuitively, the RETURN_TRUSTED_TYPE property is set to false.
+const domPurifyProfile = {
+    RETURN_TRUSTED_TYPE: false,
+    USE_PROFILES: {
+        html: true
+    }
+}
+
+const defaultPolicy = trustedTypes.createPolicy("default", {
+    createHTML: (string) => {
+        // console.warn("Please stop using 'innerHTML'.");
+        return DOMPurify.sanitize(string, domPurifyProfile);
+    }
+});
