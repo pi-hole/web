7f9e97acf38b432c8eabea2ccafaf45218ebec62
RD WebDesign <github@rdwebdesign.com.br>
2026-09-04 01:28:25 -0300

    Fix theme selection
    - Set "bs_theme" variable for all Pi-hole themes, including the high
      contrast themes.
    
    - Simplifies the theme.js code. Most of the code was removed and now we
      only set "lte-theme" localStorage key to "light", "dark" or "auto",
      based on the currently selected Pi-hole theme.
      Pi-hole themes don't use this value directly. Only AdminLTE uses it to
      switch between light and dark modes). This is only useful for "auto"
      theme. All other themes use a single mode (always light or dark),
      regardless of the preference set on the OS or browser.
      Even so, AdminLTE always tries to read this value from localStorage on
      every page load. When no value is found, AdminLTE forces the theme to
      use OR preferences, even if the current theme has a different mode.
      Storing the theme mode in "lte-theme" localStorage key ensures that
      AdminLTE will never change "data-bs-theme" attribute on the "<html>"
      tag and will always use the correct mode for the selected theme, but
      still keeps the ability to use the preferred light/dark mode for the
      "Auto" theme.
    
    Signed-off-by: RD WebDesign <github@rdwebdesign.com.br>

--- scripts/js/theme.js
+++ scripts/js/theme.js
@@ -7,19 +7,5 @@
 
 "use strict";
 
-const currentBsTheme = document.documentElement.dataset.bsTheme;
-
-const setAutoTheme = () => {
-  const systemTheme = matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-
-  // Set data-bs-theme attribute for specific theme elements
-  document.querySelector(".app-header .user-menu .dropdown-menu").dataset.bsTheme = systemTheme;
-};
-
-if (currentBsTheme === "auto") {
-  setAutoTheme();
-  // Listen for OS theme changes
-  matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
-    setAutoTheme();
-  });
-}
+// Force AdminLTE to use light/dark theme based on the current selected Pi-hole theme
+localStorage.setItem("lte-theme", document.documentElement.dataset.bsTheme);
